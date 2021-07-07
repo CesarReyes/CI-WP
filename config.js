36b21d4295e35e7112d4e4d10298d181a8b93790
Cesar <CesarReyes@users.noreply.github.com>
2021-07-07 15:52:30 -0500

    Remove unused and old files

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'r99giJLGZ559nptkuVFZog'
-  , consumer_secret: 'bVt7HetK0k6SK7tiF4yntZBKtd1rmUeCWsWJ8WadZOE'
-  , access_token: '342257644-ekcvFYgLtAuUVucAUQUije9O8dKqAl8rJ28nqWU1'
-  , access_token_secret: 'u4wdiqm44WyIOM6xgB7A9RMryGgUoDOeJK1vwvWA'
-}
